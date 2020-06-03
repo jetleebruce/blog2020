6d17bfc97fe8b1771c6f0238e7ec844ee3b16c48
Serge Modin <modinsv@yandex.ru>
2020-06-03 10:42:01 +0300

    Trendin Featured Posts Schemas

---
+++ graphql/schemas/posts/mocks/featured.js
@@ -0,0 +1,35 @@
+const moment = require("moment");
+
+module.exports = [
+  {
+    title: "Can anyone code?",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech cultures", "Tech News"],
+    link: "#",
+    image: "n10.jpg",
+  },
+
+  {
+    title: "Using WAS 3 to manage storage",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Cloud"],
+    link: "#",
+    image: "n10.jpg",
+  },
+
+  {
+    title: "Popular programming languages in 2020",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech cultures", "Tech News"],
+    link: "#",
+    image: "n10.jpg",
+  },
+
+  {
+    title: "Brain Hack how to do things faster",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Brian Health"],
+    link: "#",
+    image: "n10.jpg",
+  },
+];
---
+++ graphql/schemas/posts/mocks/trending.js
@@ -0,0 +1,43 @@
+const moment = require("moment");
+
+module.exports = [
+  {
+    title: "GraphQl vs REST API",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["React", "JavaScript"],
+    link: "#",
+    image: "n6.jpg",
+  },
+
+  {
+    title: "A day in life",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech Cultures"],
+    link: "#",
+    image: "n6.jpg",
+  },
+
+  {
+    title: "WebDev salary in 2020",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech News"],
+    link: "#",
+    image: "n6.jpg",
+  },
+
+  {
+    title: "Brain Hack how to do things faster",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Brian Health"],
+    link: "#",
+    image: "n6.jpg",
+  },
+
+  {
+    title: "React vs Vue",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["React", "JavaScript"],
+    link: "#",
+    image: "n6.jpg",
+  },
+];
